40fffa5ea55ef12cf2a6cb9fc342b78a93b9a46e
aayad <abdelrahman.ayad@gmail.com>
2026-05-22 12:32:52 -0600

    update dir from augur_data to PRAS

---
+++ reeds/hpc/srun_template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=[your HPC allocation]
+#SBATCH --time=2-00:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=[your email address]
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mem=246000        # RAM in MB; up to 246000 for normal or 2000000 for bigmem on kestrel
+# add >>> #SBATCH --qos=high <<< above for quicker launch at double AU cost
